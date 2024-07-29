da1dccba084c44518042973f7c7e8d727dbb51de
Januprasad <jnuprasad@gmail.com>
2024-07-29 11:17:11 +0530

    base project

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SearchRecipieApp"
+include(":app")
+include(":common")
+include(":data")
+include(":domain")
+include(":db")
+include(":features")
